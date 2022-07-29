427cabcc0080ff5cae8b77d2f74d76ad3f6672e1
sho <sho@shonoMacBook-Air.local>
2022-07-29 23:27:36 +0900

    マイグレーション作成

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,4 +7,3 @@
   <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
   <title>Document</title>
 </head>
-<body>
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+Route::get('/', function () {
+    return view('users.index');
+});
 
 require __DIR__.'/auth.php';
